5e90844437fea4ebfc7c81b253cf8f0c17cd5655
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 16:22:19 -0700

    Initial Commit

---
+++ indeterminate-progress-view/indeterminate_progress_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  indeterminate_progress_viewApp.swift
+//  indeterminate-progress-view
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct indeterminate_progress_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
